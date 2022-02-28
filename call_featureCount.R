6a97ef5614d81d8c50c4899246dff586a9491ba6
Melina Claussnitzer Lab <83827152+ClaussnitzerLab@users.noreply.github.com>
2022-02-28 12:04:56 -0500

    Call the R feature count function
    init

---
+++ call_featureCount.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+library("Rsubread")
+args = commandArgs(trailingOnly=TRUE)
+input_bam = args[1]
+input_annot_inbuilt = args[2]
+input_gtf = args[3]
+input_featureType = args[4]
+input_attrType = args[5]
+input_strandSpecific = args[6]
+input_isGTFAnnotationFile = args[7]
+input_isPairedEnd = args[8]
+
+fc <- featureCounts(files=input_bam, annot.inbuilt=input_annot_inbuilt, 
+annot.ext=input_gtf,GTF.featureType=input_featureType,
+GTF.attrType=input_attrType, strandSpecific=input_strandSpecific, isGTFAnnotationFile=input_isGTFAnnotationFile,
+isPairedEnd=input_isPairedEnd)
+write.csv(fc$counts, "feature_counts.csv")

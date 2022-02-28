0b9c7939330993e03d3aadb91eeb4330df91f9b1
Melina Claussnitzer Lab <83827152+ClaussnitzerLab@users.noreply.github.com>
2022-02-28 15:07:41 -0500

    Update call_featureCount.R
    type conversion

--- call_featureCount.R
+++ call_featureCount.R
@@ -5,9 +5,18 @@ input_annot_inbuilt <- args[2]
 input_gtf <- args[3]
 input_featureType <- args[4]
 input_attrType <- args[5]
-input_strandSpecific <- args[6]
-input_isGTFAnnotationFile <- args[7]
-input_isPairedEnd <- args[8]
+input_strandSpecific <- as.numeric(args[6])
+if (args[7] == "TRUE"){
+    input_isGTFAnnotationFile <- TRUE
+}else{
+    input_isGTFAnnotationFile <- FALSE
+}
+
+if (args[8] == "TRUE"){
+    input_isPairedEnd <- TRUE
+}else{
+    input_isPairedEnd <- FALSE
+}
 
 fc <- featureCounts(files=input_bam, annot.inbuilt=input_annot_inbuilt, annot.ext=input_gtf,GTF.featureType=input_featureType, GTF.attrType=input_attrType, strandSpecific=input_strandSpecific, isGTFAnnotationFile=input_isGTFAnnotationFile, isPairedEnd=input_isPairedEnd)
 write.csv(fc$counts, "feature_counts.csv")
